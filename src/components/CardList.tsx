76fa12b28cf0ee56f208c363d878a086083b99a5
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-22 11:51:53 +0100

    feat(cardlist.tsx): add IRobot interface to the props

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Card from './Card';
-
-const CardList = ({ robots }) => {
+import { IRobot } from '../containers/App';
+ 
+const CardList = ({ robots }: {robots: Array<IRobot>}) => {
   return (
     <div>
       {
